ba29e0e5dcae8332ba7c878165f006dc18e3e07e
unknown <zienzidan41@gmail.com>
2023-11-03 00:23:11 +0700

    Coba CI4

---
+++ heloo-CI/app/Views/halo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Code Igniter 4</title>
+</head>
+
+<body>
+    <?php
+    echo "<h1>Hello World</h1><br>";
+    echo "<h2>Aku Zidan dan sedang mencoba Code Igniter</h2><br>";
+    echo "<h2>Hari ini tanggal " . date("Y-m-d") . "<h2><br>";
+    ?>
+</body>
+
+</html>
